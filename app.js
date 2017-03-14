c60d2346e2d761e473fdfc4ef0b5a4e2160f7b83
Mathew C Martin <chactas.stryker@gmail.com>
2017-03-13 23:49:31 -0700

    Third commit... no mas

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 'use strict';
 
-var cookieShop = {
+var storeHours = ['6AM', '7AM', '8AM', '9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM', '6PM', '7PM', '8PM', 'Total'];
+
+var firstnpike = {
   storeName: '1st and Pike',
   minCust: 23,
   maxCust: 65,
@@ -16,10 +18,21 @@ var cookieShop = {
       console.log('sales is: ' + this.hourlySales);
       total = total + this.hourlySales[i];
       console.log('total is: ' + total);
+
     }
   }
 };
-var cookieShop = {
+var body = document.getElementsByTagName('body')[0];
+var firstList = document.createElement('ul');
+var firstnpikeHourly = firstnpike.storeSales();
+console.log(firstnpikeHourly);
+for (var i = 0; i < storeHours.length; i++){
+  var firstlistItem = document.createElement('li');
+  firstlistItem.textContent = firstnpike[1];
+};
+body.appendChild(firstList);
+
+var seatacAirport = {
   storeName: 'SeaTac Airport',
   minCust: 3,
   maxCust: 24,
@@ -38,7 +51,17 @@ var cookieShop = {
     }
   }
 };
-var cookieShop = {
+var body = document.getElementsByTagName('body')[0];
+var firstList = document.createElement('ul');
+var seatacAirportHourly = seatacAirport.storeSales();
+console.log(seatacAirportHourly);
+for (var i = 0; i < storeHours.length; i++){
+  var firstlistItem = document.createElement('li');
+  firstlistItem.textContent = seatacAirport[1];
+};
+body.appendChild(firstList);
+
+var seattleCenter = {
   storeName: 'Seattle Center',
   minCust: 11,
   maxCust: 38,
@@ -57,7 +80,17 @@ var cookieShop = {
     }
   }
 };
-var cookieShop = {
+var body = document.getElementsByTagName('body')[0];
+var firstList = document.createElement('ul');
+var seattleCenterHourly = seattleCenter.storeSales();
+console.log(seattleCenterHourly);
+for (var i = 0; i < storeHours.length; i++){
+  var firstlistItem = document.createElement('li');
+  firstlistItem.textContent = seattleCenter[1];
+};
+body.appendChild(firstList);
+
+var capitolHill = {
   storeName: 'Capitol Hill',
   minCust: 20,
   maxCust: 38,
@@ -76,7 +109,17 @@ var cookieShop = {
     }
   }
 };
-var cookieShop = {
+var body = document.getElementsByTagName('body')[0];
+var firstList = document.createElement('ul');
+var capitolHillHourly = capitolHill.storeSales();
+console.log(capitolHillHourly);
+for (var i = 0; i < storeHours.length; i++){
+  var firstlistItem = document.createElement('li');
+  firstlistItem.textContent = capitolHill[1];
+};
+body.appendChild(firstList);
+
+var alki = {
   storeName: 'Alki',
   minCust: 2,
   maxCust: 16,
@@ -95,5 +138,12 @@ var cookieShop = {
     }
   }
 };
-
-var storeHours = ['6AM', '7AM', '8AM', '9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM', '6PM', '7PM', '8PM', 'Total'];
+var body = document.getElementsByTagName('body')[0];
+var firstList = document.createElement('ul');
+var alkiHourly = alki.storeSales();
+console.log(alkiHourly);
+for (var i = 0; i < storeHours.length; i++){
+  var firstlistItem = document.createElement('li');
+  firstlistItem.textContent = alki[1];
+};
+document.body.appendChild(firstList);
