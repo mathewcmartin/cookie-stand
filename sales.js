bb5630cbb051657cc50cc4e5e761a65fc6356626
Mathew C Martin <chactas.stryker@gmail.com>
2017-03-15 22:45:54 -0700

    Final commit day 8

--- sales.js
+++ sales.js
@@ -3,12 +3,19 @@
 var storeHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm', 'total'];
 
 var storeList = [];
+
+var firstnpike = new StoreName('1st and Pike', 23, 65, 6.3);
+var seatacAirport = new StoreName('SeaTac Airport', 3, 24, 1.2);
+var seattleCenter = new StoreName('Seattle Center', 11, 38, 3.7);
+var capitolHill = new StoreName('Capitol Hill', 20, 38, 2.3);
+var alki = new StoreName('Alki', 2, 16, 4.6);
+
 var body = document.getElementsByTagName('body')[0];
 
 function StoreName(sName, minCust, maxCust, avgSales) {
-  this.store = (sName);
-  this.minCust = Math.floor(minCust);
-  this.maxCust = Math.floor(maxCust);
+  this.store = sName;
+  this.minCust = minCust;
+  this.maxCust = maxCust;
   this.avgSales = avgSales;
   this.hourlySalesArr = [];
   this.randomCust = function(){
@@ -17,52 +24,75 @@ function StoreName(sName, minCust, maxCust, avgSales) {
   };
   this.storeSales = function(){
     var total = 0;
-    for (var i = 0; i < storeHours.length - 1; i++) {
+    for (var i = 0; i < storeHours.length - 1; i++){
       var cookiesHourly = Math.floor(this.randomCust * this.avgSales);
       this.hourlySalesArr.push(cookiesHourly);
       total += cookiesHourly;
       console.log(cookiesHourly);
+      console.log('this.storeSales');
     }
     this.hourlySalesArr.push(total);
+    console.log(this.hourlySalesArr);
   };
   this.createTblFunc = function(){
     this.storeSales();
-    var tbl = document.getElementsByTagName('tbl')[0];
-    var tbody = document.createElement('tbody');
-    tbl.appendChild(tbody);
+    var tbody = document.getElementsByTagName('tbody')[0];
     var tr = document.createElement('tr');
     tbody.appendChild(tr);
     var tblData = document.createElement('td');
-    tr.appendChild(td);
+    tr.appendChild(tblData);
     tblData.innerText = this.store;
-    for (var i = 0; i < storeHours.length; i++) {
+    for (var i = 0; i < storeHours.length; i++){
       var td = document.createElement('td');
       td.innerText = this.hourlySalesArr[i];
       tr.appendChild(td);
     }
-    tbl.appendChild(tbody);
-  }
+    tblData.appendChild(tbody);
+  };
   storeList.push(this);
 }
 
-function renderHTML () {
+function renderHTML() {
   var table = document.createElement('table');
   body.appendChild(table);
   var thead = document.createElement('thead');
   table.appendChild(thead);
+  var tbody = document.createElement('tbody');
+  thead.appendChild(tbody);
   var tr = document.createElement('tr');
-  thead.appendChild(tr);
+  table.appendChild(tr);
   var td = document.createElement('td');
   td.innerText = 'StoreName';
   thead.appendChild(td);
   for(var i = 0; i < storeHours.length; i++) {
-    var tblHead = document.createElement('tblHead');
+    var tblHead = document.createElement('th');
     tblHead.innerText = storeHours[i];
     thead.appendChild(tblHead);
   }
   renderHTML();
-};/*
+};
 var newStoreForm = document.getElementById('newStoreInputForm');
+function newStoreName(event){
+  event.preventDefault();
+  var storeEntryForm = event.target;
+  console.log('storeEntryForm');
+  var newStoreName = storeEntryForm.elements['sName'].value;
+  console.log('newStoreForm logic');
+  var newMinCustValues = storeEntryForm.elements['minCust'].value;
+  var newMaxCustValues = storeEntryForm.elements['maxCust'].value;
+  var newAvgSalesValues = storeEntryForm.elements['avgSales'].value;
+  form.getElementById('newStoreInputForm').reset();
+}
+document.addEventListener('submit', newStoreName);
+
+/*
+document.getElementsByTagName('newStoreName');
+document.getElementsByTagName('newMinCust');
+document.getElementsByTagName('newMaxCust');
+document.getElementsByTagName('newAvgSales');
+
+/*
+var newStoreForm = form.getElementById('newStoreInputForm');
 function newStoreName(event){
   event.preventDefault();
   var storeEntryForm = event.target;
@@ -72,14 +102,14 @@ function newStoreName(event){
   var newMinCustValues = storeEntryForm.elements['minCust'].value;
   var newMaxCustValues = storeEntryForm.elements['maxCust'].value;
   var newAvgSalesValues = storeEntryForm.elements['avgSales'].value;
-  document.getElementById('newStoreInputForm').reset();
+  form.getElementById('newStoreInputForm').reset();
 }
 newStoreForm.addEventListener('submit', newStoreName);
   /*
 sName, minCust, maxCust, avgSales
 'use strict';
 
-var form = document.getElementById('the-form');
+var form = form.getElementById('the-form');
 function alertTheUser(event){
   event.preventDefault(); // stops the form from submitting and leaving the page.
   // time for the harvest
@@ -89,7 +119,7 @@ function alertTheUser(event){
 };
 form.addEventListener('submit', alertTheUser);
 
-var firstname = document.getElementById('firstname');
+var firstname = form.getElementById('firstname');
 function capitalizeEverything(event){
   // var theText = this.value;
   this.value = "You've focused on me!";
@@ -98,17 +128,14 @@ function capitalizeEverything(event){
 }
 firstname.addEventListener('focus', capitalizeEverything);
 
-  document.getElementById('newStoreName')
-  document.getElementById('newMinCust')
-  document.getElementById('newMaxCust')
-  document.getElementById('newAvgSales')
+  form.getElementById('newStoreName')
+  form.getElementById('newMinCust')
+  form.getElementById('newMaxCust')
+  form.getElementById('newAvgSales')
 }
 
-
-document.getElementById('form1').addEventListener('submit', function(evt){
+form.getElementById('form1').addEventListener('submit', function(evt){
     evt.preventDefault();
-
-
 
 /*
 var firstnpike = new StoreName('1st and Pike', 23, 65, 6.3);
@@ -142,12 +169,12 @@ var seatacAirport = {
     }
   }
 };
-var body = document.getElementsByTagName('body')[0];
-var firstList = document.createElement('ul');
+var body = form.getElementsByTagName('body')[0];
+var firstList = form.createElement('ul');
 var seatacAirportHourly = seatacAirport.storeSales();
 console.log(seatacAirportHourly);
 for (var i = 0; i < storeHours.length; i++){
-  var firstlistItem = document.createElement('li');
+  var firstlistItem = form.createElement('li');
   firstlistItem.textContent = seatacAirport[1];
 };
 body.appendChild(firstList);
@@ -171,12 +198,12 @@ var seattleCenter = {
     }
   }
 };
-var body = document.getElementsByTagName('body')[0];
-var firstList = document.createElement('ul');
+var body = form.getElementsByTagName('body')[0];
+var firstList = form.createElement('ul');
 var seattleCenterHourly = seattleCenter.storeSales();
 console.log(seattleCenterHourly);
 for (var i = 0; i < storeHours.length; i++){
-  var firstlistItem = document.createElement('li');
+  var firstlistItem = form.createElement('li');
   firstlistItem.textContent = seattleCenter[1];
 };
 body.appendChild(firstList);
@@ -200,12 +227,12 @@ var capitolHill = {
     }
   }
 };
-var body = document.getElementsByTagName('body')[0];
-var firstList = document.createElement('ul');
+var body = form.getElementsByTagName('body')[0];
+var firstList = form.createElement('ul');
 var capitolHillHourly = capitolHill.storeSales();
 console.log(capitolHillHourly);
 for (var i = 0; i < storeHours.length; i++){
-  var firstlistItem = document.createElement('li');
+  var firstlistItem = form.createElement('li');
   firstlistItem.textContent = capitolHill[1];
 };
 body.appendChild(firstList);
@@ -229,14 +256,14 @@ var alki = {
     }
   }
 };
-var body = document.getElementsByTagName('body')[0];
-var firstList = document.createElement('ul');
+var body = form.getElementsByTagName('body')[0];
+var firstList = form.createElement('ul');
 var alkiHourly = alki.storeSales();
 console.log(alkiHourly);
 for (var i = 0; i < storeHours.length; i++){
-  var firstlistItem = document.createElement('li');
+  var firstlistItem = form.createElement('li');
   firstlistItem.textContent = alki[1];
 };
-document.body.appendChild(firstList);
+form.body.appendChild(firstList);
 
 */
